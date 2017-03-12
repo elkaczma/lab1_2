44564b9b8052bb4c1f974a2eaac614edf4b7c06f
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-12 14:48:52 +0100

    #2 Utworzenie klasy PaymentFactory z metoda createPayment

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactory.java
@@ -0,0 +1,14 @@
+package pl.com.bottega.ecommerce.sales.domain.payment;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentFactory {
+	
+	public Payment createPayment(ClientData clientData, Money amount) {
+		Id id = Id.generate();
+
+		return new Payment(id, clientData, amount.multiplyBy(-1));	
+	}
+}
